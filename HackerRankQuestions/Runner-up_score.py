7f8209d0d2f148af53e5e2022482666780ce0654
Jayanto-stack <jayantoms.007@gmail.com>
2025-07-10 18:02:03 +0530

    Runner-up_score.py

--- HackerRankQuestions/Runner-up_score.py
+++ HackerRankQuestions/Runner-up_score.py
@@ -3,3 +3,8 @@ Given the participant's score sheet for your University Sports Day,
 you are required to find the runner-up score.
 
 '''
+if __name__ == '__main__':
+    n = int(input())
+    arr = map(int, input().split())
+
+
