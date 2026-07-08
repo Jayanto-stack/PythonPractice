d2199610ea8995b5d07950d92401504f5317d990
JSMSec <77456122+Jayanto-stack@users.noreply.github.com>
2026-07-08 08:03:16 +0000

    changes in Basic file

--- Basic.py
+++ Basic.py
@@ -16,6 +16,30 @@ full_name = first_name + " "+ last_name
 print(full_name)
 """
 # Adding whitespace to strings with tabs or newlines
+"""
 print("Python")
 print("\tPython")
 print("Languages:\n\tPython\n\tC\n\tJavascript")
+"""
+favourite_language = 'python '
+favourite_language.rstrip()
+print(favourite_language)
+
+# Integer 
+a = 2
+b = 3
+c = a * b
+print(c)
+
+# Float numbers
+"""
+z = 0.4
+f = 3.5
+x = z + f
+print(x)
+"""
+age = 24
+message = "Happy" +" "+ str(age) + "th Birthday!"
+print(message)
+
+
