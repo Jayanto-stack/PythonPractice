179f96495f15200960f5dd5dbb377a5bf142ff85
JSMSec <77456122+Jayanto-stack@users.noreply.github.com>
2026-07-08 06:37:58 +0000

    Changes in file

---
+++ Basic.py
@@ -0,0 +1,21 @@
+
+# String
+"""
+name = "jayanto mukhopadhyay"
+print(name.title())
+# Using title method changing case in a String
+# lower(), Upper()
+print(name.upper())
+print(name.lower())
+"""
+# Combining or Concatenating Strings
+"""
+first_name = "jayanto"
+last_name = "mukhopadhyay"
+full_name = first_name + " "+ last_name
+print(full_name)
+"""
+# Adding whitespace to strings with tabs or newlines
+print("Python")
+print("\tPython")
+print("Languages:\n\tPython\n\tC\n\tJavascript")
