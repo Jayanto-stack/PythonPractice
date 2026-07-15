6fbc721dd335cc544fd5dacdc35c9fe80207e3cb
JSMSec <77456122+Jayanto-stack@users.noreply.github.com>
2026-07-15 09:43:48 +0000

    few changes

--- Practice.py
+++ Practice.py
@@ -17,7 +17,7 @@ print(f"Dear {guest[1].title()}, you are invited to the wedding aniversary of my
 print(f"Dear {guest[2].title()}, you are invited to the wedding aniversary of my brother.")
 print(f"Dear {guest[3].title()}, you are invited to the wedding aniversary of my brother.")
 """
-# Question 2
+# Question 2 guess the output of the below code
 student = "Rahul"
 marks = 80
 print(f"{student} scored {marks} marks.")
