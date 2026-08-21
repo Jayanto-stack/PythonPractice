a44e71f0e75caa832c367b9fe0b8ca16a5a1aedf
JSMSec <77456122+Jayanto-stack@users.noreply.github.com>
2026-08-21 07:47:56 +0000

    dictionay added some code

--- Functions.py
+++ Functions.py
@@ -78,9 +78,31 @@ describe_pet('dog', 'dobby')
 describe_pet('hamster', 'harry')
 """
 # Keyword Arguments
+"""
 def describe_pet(animal_type, pet_name):
     # Display information about a pet.
     print("\nI have a " + animal_type + ".")
     print("My " + animal_type + "'s name is " + pet_name.title() + ".")
 describe_pet(animal_type='hamster', pet_name='harry')
-
+"""
+# Return 
+"""
+def get_formatted_name(first_name, last_name, middle_name=''):
+    # Return a full name, neatly formatted.
+    if middle_name:
+        full_name = first_name + ' ' + middle_name +' '+ last_name
+    else:
+        full_name = first_name + ' ' + last_name
+    return full_name.title()
+musician = get_formatted_name('lata', 'mangeshkar')
+print(musician)
+musician = get_formatted_name('rahat', 'ali khan', 'fateh')
+print(musician)
+"""
+# Return a dictionary
+def build_person(first_name, last_name):
+    # Return a dictionary of information about a person
+    person = {'first': first_name, 'last': last_name}
+    return person
+musician = build_person('Arjit', 'Singh')
+print(musician)
